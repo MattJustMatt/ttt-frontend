12ac1767ca45d48269869c13ef0aa5d2368f31b0
Matthew Salsamendi <matthewsalsamendi@gmail.com>
2023-04-14 04:08:16 -0700

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  <GoogleAnalytics trackPageViews />
   return <Component {...pageProps} />;
 };
 
